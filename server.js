027f8e4f51c9668d828f965c4f512a819d84f9a3
Jose <sixgg4@gmail.com>
2019-04-25 20:35:24 -0400

    Stripping all css, reworking html structure and static files

--- server.js
+++ server.js
@@ -1,33 +1,30 @@
-// const mongooose = require('mongooose')
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const favicon = require('express-favicon');
-const app = express();
-const PORT = 8000;
-
+// const mongoose = require('mongoose')
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const favicon = require('express-favicon')
+const app = express()
+const PORT = 8000
 
 /* Templating engine, views and static files  */
-app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/client/public/views'));
-app.use(express.static(path.join(__dirname, 'client/public/static/css')));
-app.use(express.static(path.join(__dirname, 'client/public/static/imgs')));
-app.use(express.static(path.join(__dirname, 'client/public/static/js')));
-app.use(favicon(path.join(__dirname + '/client/public/static/favicon.png')));
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, '/client/public/assets/css')))
+app.use(express.static(path.join(__dirname, '/client/public/assets/js')))
+app.use(express.static(path.join(__dirname, '/client/public/assets/js/lib')))
+app.use(express.static(path.join(__dirname, '/client/public/assets/imgs')))
+app.use(favicon(path.join(__dirname, '/client/public/assets/favicon.png')))
+app.set('views', path.join(__dirname, '/client/public/views'))
 
 /* Middleware for forms */
 
 /* Importing the routes as a controller,passing in express "app" */
-// require("./server/config/mongooose.js");
-require("./server/config/mongooose.js");
-require('./server/config/routes')(app);
+// require("./server/config/mongoose.js");
+require('./server/config/mongoose.js')
+require('./server/config/routes')(app)
 /* imports mongoose from the mongoose.js */
-
-
-
 
 /* Server */
 app.listen(PORT, () => {
-    console.log(`Listening on port: ${PORT}`);
-});
+  console.log(`Listening on port: ${PORT}`)
+})
---
+++ server/config/mongoose.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const path = require('path')
+const fs = require('fs')
+const models = path.join(__dirname, '../models')
+
+/* connect (if not create a new) to our DATABASE */
+mongoose.connect('mongodb://test:test12345@ds117773.mlab.com:17773/todo-app', {
+  useNewUrlParser: true
+})
+
+/* Loading the Schema "Model" available in the models folder */
+fs.readdirSync(models).forEach(function (file) {
+  // console.log('From Mongoose.js --> Model Loaded: ' + file);
+  if (file.indexOf('.js') >= 0) {
+    require(models + '/' + file)
+  }
+})
