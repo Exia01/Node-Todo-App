0a13a7b9792695ad21c85da8d2167896f68a5806
Jose <sixgg4@gmail.com>
2019-05-05 18:56:28 -0400

    minor changes grammar

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,19 +1,30 @@
-const mongoose = require('mongoose')
-const path = require('path')
-const fs = require('fs')
-const models = path.join(__dirname, '../models')
+const mongoose = require('mongoose');
+const path = require('path');
+const fs = require('fs');
+const models = path.join(__dirname, '../models');
 
 /* connect (if not create a new) to our DATABASE */
 // mongoose.connect('mongodb://test:test12345@ds117773.mlab.com:17773/todo-app', {
 //   useNewUrlParser: true
 // })
-mongoose.connect('mongodb+srv://admin:Password01@main-cluster-hpysy.mongodb.net/test?retryWrites=true', {
-  useNewUrlParser: true
-})
+mongoose
+  .connect(
+    'mongodb+srv://admin:Password01@main-cluster-hpysy.mongodb.net/test?retryWrites=true',
+    {
+      useNewUrlParser: true
+    }
+  )
+  .then(() => {
+    console.log('Connected to DB');
+  })
+  .catch(err => {
+    console.log('ERROR', err.message);
+  });
+  
 /* Loading the Schema "Model" available in the models folder */
-fs.readdirSync(models).forEach(function (file) {
+fs.readdirSync(models).forEach(function(file) {
   // console.log('From Mongoose.js --> Model Loaded: ' + file);
   if (file.indexOf('.js') >= 0) {
-    require(models + '/' + file)
+    require(models + '/' + file);
   }
-})
+});
