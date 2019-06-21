8d0e5cdc997b9472e39f6e310bac67b5d67459ff
exia01 <gonzalej@protonmail.com>
2019-06-21 12:15:24 -0400

    complete delete. refractor code

--- client/public/assets/js/index.js
+++ client/public/assets/js/index.js
@@ -1,34 +1,99 @@
 'use strict';
-document.addEventListener('DOMContentLoaded', function() {
-  let errorTag = document.getElementById('errors');
-  let renderTag = document.querySelector('#render');
-  let submitForm = document.querySelector('#main');
-  errorTag.style.display = 'none';
-
-  //axios type config
-  const config = {
-    headers: {
-      accept: 'application/json',
-      'Content-Type': 'application/json'
-    },
-    data: {}
-  };
+document.addEventListener('DOMContentLoaded', function () {
+  var { renderTag, errorTag, submitForm, config } = initialSetup(fadeInOrOut);
   //Todo INDEX
-  let todo_index = (async () => {
+  let todo_index = async () => {
     const response = await axios({
       url: '/api/todos',
       method: 'get'
     });
     return response;
-  })();
+  };
+  todo_index().then(result => {
+    let todos = result.data;
+    //could implement maps and document fragment
+    todos.forEach(e => {
+      let tempTag = createLi(e)
+      renderTag.appendChild(tempTag);
+    });
+  }).catch(err => {
+    let message = err.response.data;
+    generaterErrors(message, errorTag, createErrorTag);
+    errorTag.style.display = 'block';
+    fadeIn(errorTag);
+  });
 
-  todo_index.then(result => {
-      let todos = result.data;
-      //could implement maps and document fragment
-      todos.forEach(e => {
-        let tempTag = createLi(e)
+  // Todo NEW
+  submitForm.addEventListener('submit', function (e) {
+    e.stopPropagation();
+    e.preventDefault();
+    let obj = generateObj.call(this);
+    let data = JSON.stringify(obj);
+    (async () => {
+      let errToRemove = document.querySelector("h5") // removes the error tag
+      if (errToRemove) {
+        fadeInOrOut(errToRemove)
+        setTimeout(() => {
+          errorTag.removeChild(errToRemove)
+        }, 100);
+      }
+      try {
+        const response = await axios.post('/api/todos', data, config);
+        errorTag.style.display = 'block';
+        let tempTag = createLi(response.data)
         renderTag.appendChild(tempTag);
-      });
+        submitForm.reset()
+      } catch (err) {
+        errorTag.style.display = 'block';
+        if (err.response) {
+          console.log(err.response.data);
+        }
+        let message = err.response.data;
+        generaterErrors(message, errorTag, createErrorTag);
+        
+      }
+    })();
+  });
+
+
+
+  //Create Li Tag --> could implement document fragment?
+  function createLi(obj) {
+    let liTag = document.createElement('li');
+    let spanTag = document.createElement('span');
+    let iTag = document.createElement('i');
+    liTag.addEventListener("click", toggleListener)
+    liTag.setAttribute('id', `${obj._id}`);
+    // liTag.classList.add('visible')
+    spanTag.addEventListener("click", removeLi)
+    spanTag.appendChild(iTag);
+    iTag.setAttribute('class', `fas fa-trash`);
+    liTag.appendChild(spanTag);
+    liTag.appendChild(document.createTextNode(`${obj.item}`));
+    return liTag
+  }
+  function createErrorTag(obj) {
+    let newTag = document.createElement("h5")
+    newTag.setAttribute("class", "text-center")
+    newTag.classList.add('hidden')
+    newTag.appendChild(document.createTextNode(`${
+      obj.error.item.message
+      }`))
+    return newTag
+  }
+
+  //Remove Li Tag 
+  function removeLi() {
+    let _id = this.parentNode.id
+    const deleteReq = async function () {
+      let response = await axios.delete(`/api/todos/${_id}`, config)
+      return response
+    }
+    deleteReq().then(data => {
+      this.parentNode.classList.toggle("hidden")
+      setTimeout(() => {
+        renderTag.removeChild(this.parentNode)
+      }, 1000);
     }).catch(err => {
       let message = err.response.data;
       function* generate(message) {
@@ -41,45 +106,24 @@ document.addEventListener('DOMContentLoaded', function() {
       fadeIn(errorTag);
     });
 
-  // Todo NEW
-  submitForm.addEventListener('submit', function (e) {
-    e.stopPropagation();
-    e.preventDefault();
-    let submitFormData = new FormData(this);
-    let obj = {};
-    //creating json obj from form
-    submitFormData.forEach((value, key) => {
-      obj[key] = value;
-    });
-    let data = JSON.stringify(obj);
-    (async () => {
-      let errToRemove = document.querySelector("h5") // removes the error tag
-      if (errToRemove) {
-        errorTag.removeChild(errToRemove)
-      }
-      try {
-        const response = await axios.post('/api/todos', data, config);
-        errorTag.style.display = 'block';
-        let tempTag = createLi(response.data)
-        renderTag.appendChild(tempTag);
-        submitForm.reset()
-      } catch (err) {
-        console.log(err)
-        errorTag.style.display = 'block';
-        if (err.response) {
-          console.log(err.response.data);
-        }
-        let message = err.response.data;
-        function* generate(message) {
-          yield message;
-        }
-        for (let key of generate(message)) {
-          let newTag = createErrorTag(key)
-          errorTag.appendChild(newTag)
-        }
-      }
-    })();
-  });
+  }
+
+  function fadeInOrOut(el) {
+    console.log("hello?")
+    if (el.classList.contains("hidden")) {
+      el.classList.remove("hidden")
+      el.classList.add("visible")
+    } else {
+      el.classList.remove("visible")
+      el.classList.add("hidden")
+    }
+  }
+
+
+  //Complete item click toggle
+  function toggleListener() {
+    this.classList.toggle("completed")
+  }
 
   //TODO Destroy/Delete
 
@@ -92,68 +136,57 @@ document.addEventListener('DOMContentLoaded', function() {
   //   // errorTag.removeChild(errorTag.getElementsByTagName('h5'));
   // });
 
-  
-  
-  //Create Li Tag 
-  function createLi(obj) {
-    let liTag = document.createElement('li');
-    liTag.setAttribute("class", "incomplete")
-    let spanTag = document.createElement('span');
-    let iTag = document.createElement('i');
-    liTag.addEventListener("click", toggleListener)
-    spanTag.addEventListener("click", removeLi)
-    iTag.setAttribute('class', `fas fa-trash`);
-    liTag.setAttribute('id', `${obj._id}`);
-    spanTag.appendChild(iTag);
-    liTag.appendChild(spanTag);
-    liTag.appendChild(document.createTextNode(`${obj.item}`));
-    return liTag
-  }
-  function createErrorTag(obj) {
-    let newTag = document.createElement("h5")
-    newTag.setAttribute("class", "text-center")
-    newTag.classList.add('visible')
-    newTag.appendChild(document.createTextNode(`${
-      obj.error.item.message
-      }`))
-    return newTag
+
+  //Takes form data into obj
+  function generateObj() {
+    let submitFormData = new FormData(this);
+    let obj = {};
+    //creating json obj from form
+    submitFormData.forEach((value, key) => {
+      obj[key] = value;
+    });
+    return obj;
   }
 
-
-
-/* Listeners */
-  
-  //Complete item click toggle
-  function toggleListener() {
-    this.classList == 'incomplete' ? (this.classList= "completed"): (this.classList= "incomplete") ; 
-  }
-  //Remove Li Tag 
-  function removeLi() {
-    console.log(this.parentNode.id)
-     let _id = this.parentNode.id
-    const deleteReq = async function() {
-      let response = await axios.delete(`/api/todos/${_id}`, config)
-      return response
+  //cycles through data and generates tag
+  function generaterErrors(message, errorTag, createErrorTag) {
+    function* generate(message) {
+      yield message;
     }
-    deleteReq().then(data => {
-      console.log(data)
-    })
-  }
-
-  function fadeInOrOut(el) {
-    if (el.classList.contains("hidden")) {
-      el.classList.remove("hidden")
-      el.classList.add("visible")
-    } else {
-      el.classList.remove("visible")
-      el.classList.add("hidden")
+    for (let key of generate(message)) {
+      let tag = createErrorTag(key)
+      errorTag.appendChild(tag);
+      setTimeout(() => {
+        fadeInOrOut(tag)
+      }, 250);
     }
   }
 });
 
-
-
-
+function initialSetup(fadeInOrOut) {
+  let errorTag = document.getElementById('errors');
+  let renderTag = document.querySelector('#render');
+  let submitForm = document.querySelector('#main');
+  let iconTag = document.querySelector(".fa-pen-square");
+  submitForm.classList.add("visible");
+  submitForm.style.display = "block";
+  errorTag.style.display = 'none';
+  iconTag.addEventListener("click", function () {
+    fadeInOrOut(submitForm);
+    setTimeout(() => {
+      submitForm.style.display == "block" ? submitForm.style.display = "none" : submitForm.style.display = "block";
+    }, 1200);
+  });
+  //axios type config
+  const config = {
+    headers: {
+      accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    data: {}
+  };
+  return { renderTag, errorTag, submitForm, config };
+}
 //How to format form obj to json
 //https://stackoverflow.com/questions/41431322/how-to-convert-formdatahtml5-object-to-json
 // I used this to figure out the this and e.id
